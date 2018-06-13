54f050b3a1313c59dc28256f2cb2bfbcd1cfa872
1HommeAzerty <carpentier.hugues@gmail.com>
2018-06-13 21:45:09 +0200

    Modified to work with products.py

--- main.py
+++ main.py
@@ -1,29 +1,88 @@
 #! F:\OC_tp\Python\P5\env\Scripts\python
 # coding: utf-8
 
-import records
+from products import Products
 
 
-dbconnection = records.Database('mysql+mysqlconnector://root@localhost/bdd_projet5?charset=utf8mb4')
 
-def print_categories(config):
-    cat = []
-    with open(config, "r") as cfile:
-        categories = [cat.strip() for cat in cfile.read().split(",")]
-        for i, category in enumerate(categories):
-            print(i+1, category)
+class Main:
+    def __init__(self):
+        self.prod = Products()
 
-def display_product():
-    products = dbconnection.query("SELECT * FROM products LIMIT 1") #ORDER BY nutriscore")
 
-    for product in products:
-        print(f'{product["name"].decode("utf-8")}\n',
-        f'Nutriscore: {product["nutriscore"].decode("utf-8")}\n',
-        f'URL: {product["url"].decode("utf-8")}\n',
-        'Barcode: ', product['id'])
+    def start(self, config):
+        with open(config, "r") as cfile:
+            categories = [cat.strip() for cat in cfile.read().split(",")]
+        choices = categories + ["consulter les favoris", "quitter"]
+        choice = self.input(choices, "Quel est votre choix ? ")
+        self.display_products_de(choice)
 
-display_product()
-print_categories('config.txt')
+    def input(self, elements, message):
+
+        while True:
+            for i, element in enumerate(elements):
+                print(i+1, element)
+            choice = input(message)
+            if choice.isdigit():
+                choice = int(choice)
+                if 0 < choice < i:
+                    return elements[choice-1]
+
+    def display_products_de(self, category):
+        productsde = self.prod.get_DE_products_by_category(category)
+        productsde = [f"{product['name'].decode('utf-8')} ({product['nutriscore'].decode('utf-8')})" for product in productsde.all(as_dict = True)] 
+        choices = productsde + ["consulter les favoris", "quitter"]
+        choice = self.input(choices, "Quel est votre choix ? ")
+        print(choice)
+
+    def display_products_abc(self):
+        productsabc = self.productsabc
+
+        for product in productsabc:
+            print(f'{product["name"].decode("utf-8")}\n',
+                f'Nutriscore: {product["nutriscore"].decode("utf-8")}\n')
+
+
+    def display_product(self):
+        """---"""
+        stores = self.stores
+
+        products = self.products
+        for product in products:
+            print(f'{product["name"].decode("utf-8")}\n',
+            f'Nutriscore: {product["nutriscore"].decode("utf-8")}\n',
+            f'URL: {product["url"].decode("utf-8")}\n',
+            'Barcode: ', product['id'])
+
+            for store in stores:
+                if store["product_id"] == product["id"]:
+                    print(f'{store["product_id"]}\n',
+                        f'Magasin: {store["name"].decode("utf-8")}\n')
+    
+    def display_stores(self):
+        """---"""
+        stores = self.stores
+        for store in stores:
+            print(f'{store["product_id"]}\n',
+                f'Magasin: {store["name"].decode("utf-8")}\n')
+
+def main():
+    """---"""
+    inst = Main()
+    inst.start('config.txt')
+    #inst.display_product()
+    #inst.display_stores()
+    #inst.display_products_de()
+    #inst.display_products_abc()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
 
 
 #def select():
